d476df5252d87ce0012411b63e8b3c0bcfea126c
Damian Bushong <katana@odios.us>
2020-05-14 08:34:47 +0000

    [jenkins] Automated git mirroring from cr.imson.co repository

--- src/scheduled_on.py
+++ src/scheduled_on.py
@@ -9,7 +9,7 @@
 #
 '''
 
-# pylint: disable=C0116,C0301,C0411,W0511
+# pylint: disable=C0116,C0301,C0411,W0511,W1202
 
 from datetime import datetime
 import time
@@ -24,6 +24,9 @@ LAMBDA = LambdaCore(LAMBDA_NAME)
 LAMBDA.init_ec2()
 LAMBDA.init_s3()
 LAMBDA.init_sns()
+
+class RecoveredError(Exception): # pylint: disable=C0115
+    pass
 
 def lambda_handler(event, context):
     start_time = str(int(time.time() * 1000))
@@ -45,9 +48,18 @@ def lambda_handler(event, context):
         ]))
 
         if len(instances) > 0:
+            had_failures = False
             for instance in instances:
-                LAMBDA.logger.info('Starting instance %s', instance.id)
-                instance.start()
+                LAMBDA.logger.info(f'Starting instance {instance.id}')
+
+                try:
+                    instance.start()
+                except Exception: # pylint: disable=W0703
+                    LAMBDA.logger.error(f'Failed to start instance {instance.id}', exc_info=True)
+                    had_failures = True
+
+            if had_failures:
+                raise RecoveredError('One or more instance control failures occurred')
         else:
             LAMBDA.logger.info('No instances to start.')
     except Exception:
