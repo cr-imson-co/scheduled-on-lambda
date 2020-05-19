ca3b2a59cc646eb8a71a907f24cb789967f578a5
Damian Bushong <katana@odios.us>
2020-05-19 08:33:50 +0000

    [jenkins] Automated git mirroring from cr.imson.co repository

--- src/scheduled_on.py
+++ src/scheduled_on.py
@@ -12,7 +12,6 @@
 # pylint: disable=C0116,C0301,C0411,W0511,W1202
 
 from datetime import datetime
-import time
 
 from crimsoncore import LambdaCore
 
@@ -29,10 +28,6 @@ class RecoveredError(Exception): # pylint: disable=C0115
     pass
 
 def lambda_handler(event, context):
-    start_time = str(int(time.time() * 1000))
-    log_name = f'{LAMBDA_NAME}_{start_time}.log'
-    LAMBDA.change_logfile(log_name)
-
     try:
         # pylint apparently doesn't understand that LAMBDA.ec2 is lazy-loaded.  disabling the rule for that line.
         current_hour = datetime.utcnow().strftime('%H')
@@ -64,15 +59,6 @@ def lambda_handler(event, context):
             LAMBDA.logger.info('No instances to start.')
     except Exception:
         LAMBDA.logger.error('Fatal error during script runtime', exc_info=True)
-
-        # do our best to fire off the emergency flare
-        error_log_dest = f'logs/{LAMBDA_NAME}/{log_name}'
-        with open(f'{LAMBDA.config.get_log_path()}/{log_name}', 'r') as file:
-            LAMBDA.archive_log_file(error_log_dest, file.read())
-
-        # todo: make the error notification appropriate depending on if it's stdout, local, or s3 logging...
-        LAMBDA.send_notification('error', f'λ! - {LAMBDA_NAME} lambda error notification - error logs are available at {error_log_dest}')
+        LAMBDA.send_notification('error', f'{LAMBDA_NAME} lambda error notification; reference logstream {LAMBDA.config.get_log_stream()}')
 
         raise
-    finally:
-        LAMBDA.change_logfile(f'{LAMBDA_NAME}_interim.log')
